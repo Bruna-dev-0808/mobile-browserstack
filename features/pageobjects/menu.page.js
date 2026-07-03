86cbc9835e8d29deeda79c47a8b92000c92e5fe0
Bruna-dev-0808 <brunaferraz0808@gmail.com>
2026-07-03 11:48:11 -0300

    Corrige scroll nos itens do menu

--- features/pageobjects/menu.page.js
+++ features/pageobjects/menu.page.js
@@ -2,34 +2,42 @@ const BasePage = require('./base.page');
 
 const menuItems = {
   Home: {
+    text: 'Home',
     itemSelector: '~side-menu-item-home',
     screenSelector: '~Home-screen'
   },
   Webview: {
+    text: 'Webview',
     itemSelector: '~side-menu-item-webview',
     screenSelector: '~Webview-screen'
   },
   Login: {
+    text: 'Login',
     itemSelector: '~side-menu-item-login',
     screenSelector: '~Login-screen'
   },
   Forms: {
+    text: 'Forms',
     itemSelector: '~side-menu-item-forms',
     screenSelector: '~Forms-screen'
   },
   Swipe: {
+    text: 'Swipe',
     itemSelector: '~side-menu-item-swipe',
     screenSelector: '~Swipe-screen'
   },
   Drag: {
+    text: 'Drag',
     itemSelector: '~side-menu-item-drag',
     screenSelector: '~Drag-screen'
   },
   Permissions: {
+    text: 'Permissions',
     itemSelector: '~side-menu-item-permissions',
     screenSelector: '~Permissions-screen'
   },
   Data: {
+    text: 'Data',
     itemSelector: '~side-menu-item-data',
     screenSelector: '~Data-screen'
   }
@@ -43,10 +51,24 @@ class MenuPage extends BasePage {
     }
   }
 
-  async openItem(itemName) {
+  async getVisibleItem(itemName) {
     const item = menuItems[itemName];
+    const element = await $(item.itemSelector);
+
+    try {
+      await element.waitForDisplayed({timeout: 3000});
+      return element;
+    } catch (_error) {
+      await $(`android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("${item.text}")`);
+      await element.waitForDisplayed();
+      return element;
+    }
+  }
+
+  async openItem(itemName) {
     await this.open();
-    await this.waitAndClick(item.itemSelector);
+    const item = await this.getVisibleItem(itemName);
+    await item.click();
   }
 
   async validateScreen(itemName) {
@@ -57,8 +79,7 @@ class MenuPage extends BasePage {
 
   async toggleFavorite(itemName) {
     await this.open();
-    const item = await $(menuItems[itemName].itemSelector);
-    await item.waitForDisplayed();
+    const item = await this.getVisibleItem(itemName);
 
     const rect = await item.getRect();
     await browser.action('pointer')
